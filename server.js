31ecc5f9e4444af20a7b1649ba09c8b56c58872b
JC <Jose@softbank220001001094.bbtec.net>
2018-09-20 14:31:50 -0500

    Added chatkit template.

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const Chatkit = require('pusher-chatkit-server')
 
 const app = express()
+
+const chatkit = new Chatkit.default({
+  instanceLocator: 'v1:us1:ae6b9f76-4356-486c-accf-a4d022bbe839',
+  key: 'ad5af305-a61d-4344-88c5-8b535171aa69:1XcxkNt+WNa5kaWEt2dwgkPHmX1JayPMG9H9ckF017w='
+})
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
 
+app.post('/users' ,(req,res)=>{
+  const { username } = req.body
+
+
+chatkit
+.createUser({
+  name:username,
+  id: username
+})
+.then(()=> res.sendStatus(201))
+.catch(error =>{
+  if(error.error_type === 'service/chatkit/user_already_exists'){
+    res.sendStatus(200)
+  }else{
+    res.status(error.statusCode).json(error)
+    }
+  })
+})
 const PORT = 3001
 app.listen(PORT, err => {
   if (err) {
