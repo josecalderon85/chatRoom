d17c55674c6a0fc0b9dc6fe721c8da275ed78850
JC <Jose@softbank220001001094.bbtec.net>
2018-09-22 14:41:52 -0500

    Finished backend components

--- server.js
+++ server.js
@@ -1,47 +1,47 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const Chatkit = require('pusher-chatkit-server')
+const Chatkit = require('@pusher/chatkit-server')
 
 const app = express()
 
 const chatkit = new Chatkit.default({
-  instanceLocator: 'v1:us1:ae6b9f76-4356-486c-accf-a4d022bbe839',
-  key: 'ad5af305-a61d-4344-88c5-8b535171aa69:1XcxkNt+WNa5kaWEt2dwgkPHmX1JayPMG9H9ckF017w='
+ instanceLocator: 'v1:us1:ac693f4f-a223-4fb8-9cae-d2de840aaba0',
+ key:
+   'f5dc25eb-9547-4d4f-a872-98f2f192d8d0:jPmtjs/iHd1AeJTUCmu+T4L6yb2OPNNfiPta5Cdoi/M='
 })
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.post('/users',(req, res) =>{
-  const { username } = req.body
-
-  chatkit
-  .createUser({
-    name: username,
-    id: username
-  })
-  .then(()=> res.sendStatus(201))
-  .catch(error =>{
-    if(error.error_type === 'services/chatkit/user_already_exists'){
-      res.sendStatus(200)
-    }else{
-      res.status(error.statusCode).json(error)
-    }
-  })
+app.post('/users', (req, res) => {
+ const { username } = req.body
+ chatkit
+   .createUser({
+     id: username,
+     name: username
+   })
+   .then(() => res.sendStatus(201))
+   .catch(error => {
+     if (error.error === 'services/chatkit/user_already_exists') {
+       res.sendStatus(200)
+     } else {
+       res.status(error.status).json(error)
+     }
+   })
 })
 
-app.post('/authenticate',(req, res)=>{
-  const { grant_type } = req.body
-  res.json(chatkit.authenticate({ grant_type,userId: req.query.user_id}, req.query.user_id))
+app.post('/authenticate', (req, res) => {
+ const authData = chatkit.authenticate({ userId: req.query.user_id })
+ res.status(authData.status).send(authData.body)
 })
 
 const PORT = 3001
 app.listen(PORT, err => {
-  if (err) {
-    console.error(err)
-  } else {
-    console.log(`Running on port ${PORT}`)
-  }
+ if (err) {
+   console.error(err)
+ } else {
+   console.log(`Running on port ${PORT}`)
+ }
 })
